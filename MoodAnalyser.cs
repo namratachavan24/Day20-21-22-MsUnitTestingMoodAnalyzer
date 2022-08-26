cf14fdf25ec77b25dd230e5651fde1dac406df4a
Namrata Chavan <109748579+namratachavan24@users.noreply.github.com>
2022-08-26 23:32:58 +0530

    UC1 Cheking Mood Added

---
+++ MoodAnalyser.cs
@@ -0,0 +1,25 @@
+using System;
+using System.Collections.Generic;
+using System.Linq;
+using System.Text;
+using System.Threading.Tasks;
+
+namespace Moodanalyzer
+{
+    public class MoodAnalyzer
+    {
+        public string message = "I am in Good Mood";
+        public string AnalyseMood(string msg)
+        {
+            if (this.message == msg)
+            {
+                return "GOOD";
+            }
+            else
+            {
+                return "Sad";
+            }
+
+        }
+    }
+}
