bbff1c2080e0b44b78b31ed5f126bd091791dfa3
Namrata Chavan <109748579+namratachavan24@users.noreply.github.com>
2022-08-26 23:36:22 +0530

    UC1 Mood Checking Added

---
+++ UnitTestingOne.cs
@@ -0,0 +1,23 @@
+using Microsoft.VisualStudio.TestTools.UnitTesting;
+using Moodanalyzer;
+
+namespace MoodAnalyzerTest
+{
+    [TestClass]
+    public class UnitTestingOne
+    {
+        [TestMethod]
+        public void AnalyseMood()
+        {
+            //Arrange
+            MoodAnalyzer moodAnalyzer = new MoodAnalyzer();
+            string meassage = "I am in Sad Mood";
+            string ExpectedOutput = "SAD";
+
+            string result = moodAnalyzer.AnalyseMood(meassage);
+
+            Assert.AreEqual(result, ExpectedOutput);
+
+        }
+    }
+}
